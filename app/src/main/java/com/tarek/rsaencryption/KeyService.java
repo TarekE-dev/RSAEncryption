40480b5220b76afc54df0ef6f621bb6744e76f51
Tarek <tug35668@temple.edu>
2020-02-01 21:19:01 -0500

    Added generate key pair method

--- app/src/main/java/com/tarek/rsaencryption/KeyService.java
+++ app/src/main/java/com/tarek/rsaencryption/KeyService.java
@@ -9,11 +9,22 @@ import android.os.IBinder;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.google.gson.Gson;
 
 public class KeyService extends Service {
 
+    private Gson gson = new Gson();
     private final IBinder mBinder = new BinderInstance();
-    private final String KEY_PAIR_VALUES = "KEY_PAIR_VALUES";
+    private final String ENCRYPTED_KEYS = "ENCRYPTED_KEYS";
+    private final String SAVED_KEY_PAIR_VALUES = "SAVED_KEY_PAIR_VALUES";
+    private final String USER_KEYPAIR = "USER_KEYPAIR";
+    private Set<String> keyPairs = new HashSet<>();
 
     public class BinderInstance extends Binder {
         KeyService getService(){
@@ -31,18 +42,28 @@ public class KeyService extends Service {
     }
 
     public KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-        SharedPreferences sharedPreferences = getSharedPreferences(KEY_PAIR_VALUES, MODE_PRIVATE);
-        String values = sharedPreferences.getString(KEY_PAIR_VALUES, null);
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-        return null;
+        SharedPreferences sharedPreferences = getSharedPreferences(ENCRYPTED_KEYS, MODE_PRIVATE);
+        String userKeyPair = sharedPreferences.getString(USER_KEYPAIR, null);
+        KeyPair keyPair;
+        if(userKeyPair == null) {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPair = keyPairGenerator.generateKeyPair();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(USER_KEYPAIR, keyToJson(keyPair));
+            editor.apply();
+        } else {
+            keyPair = jsonToKey(userKeyPair);
+        }
+        return keyPair;
 
     }
 
-    public class EncryptionClass {
-        public EncryptionClass(){
+    private String keyToJson(KeyPair keyPair){
+        return gson.toJson(keyPair);
+    }
 
-        }
+    private KeyPair jsonToKey(String jsonString) {
+        return gson.fromJson(jsonString, KeyPair.class);
     }
 
 }
